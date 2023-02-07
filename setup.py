22e7bc442ad06c66a94034ca24764529ce6a299c
zblurx <seigneuret.thomas@pm.me>
2023-02-07 22:20:12 +0100

    bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="dploot",
-    version="2.1.10",
+    version="2.1.11",
     author="zblurx",
     author_email="seigneuret.thomas@pm.me",
     description="DPAPI looting remotely in Python",
